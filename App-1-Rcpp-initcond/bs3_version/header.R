370ce8c9c33992df70edd810dd2fa80bb46221f9
DivadNojnarg <dgranjon@ymail.com>
2019-01-11 22:21:14 +0000

    cleanup first app bs3

--- App-1-Rcpp-initcond/bs3_version/header.R
+++
@@ -1,9 +0,0 @@
-header <- dashboardHeader(
-  title = HTML(paste0(
-    '<span class = "logo-lg">Lorenz Init</span>',
-    '<img src= "lorenz_attractor.svg">'
-  )),
-  
-  titleWidth = 300
-  
-)
--- App-1-Rcpp-initcond/bs3_version/ui.R
+++
@@ -1,8 +0,0 @@
-# Define UI 
-dashboardPage(skin = "black", title = "Lorenz initial conditions", 
-              collapse_sidebar = TRUE, header, sidebar, body,
-              footerOutput(outputId = "dynamicFooter"), 
-              div(id = "controlbar",
-                  dashboardControlbar()
-              )
-)
